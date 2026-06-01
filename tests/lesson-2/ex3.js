894d766edea26ef8088cc810006a559e3866b378
Kyla <kyla@Kylas-MacBook-Pro.local>
2026-06-01 15:33:37 +0700

    fix: update ext3, ext4, ext5

--- tests/lesson-2/ex3.js
+++ tests/lesson-2/ex3.js
@@ -1,11 +1,9 @@
 const a = 50;
 if (a > 0) {
     console.log("Giá trị bạn nhập là số dương")
-}
-if (a < 0) {
+} else if (a < 0) {
     console.log("Giá trị bạn nhập là số âm")
-}
-if (a === 0) {
+} else {
     console.log("Giá trị bạn nhập là số 0")
 }
 
--- tests/lesson-2/ex4.js
+++ tests/lesson-2/ex4.js
@@ -1,4 +1,12 @@
 const height = 165;
-console.log(
-    "Cân nặng lý tưởng:", 58.5 + "kg,", "Cân nặng tối đa:", 65 + "kg,", "Cân nặng tối thiểu:", 52 + "kg"
-)
+
+if (height > 100) {
+const leHeight = height % 100
+
+const canNangLyTuong = leHeight * 9 / 10;
+const canNangToiDa = leHeight;
+const canNangToiThieu = leHeight * 8 / 10;
+console.log( `Cân nặng lý tưởng của bạn là: ${canNangLyTuong} kg, Cân nặng tối đa của bạn là: ${canNangToiDa} kg, Cân nặng tối thiểu của bạn là: ${canNangToiThieu} kg` )
+} else {
+    console.log("Chiều cao của bạn phải lớn hơn 100cm")
+}
